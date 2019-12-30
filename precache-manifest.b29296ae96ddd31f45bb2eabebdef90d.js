1a76bdc1ca3a73bc7a7653058ffd25ec99ac59b8
rizwanishaq <rizwanishaq@gmail.com>
2019-12-30 11:26:19 +0100

    Updates

---
+++ precache-manifest.b29296ae96ddd31f45bb2eabebdef90d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc1f757e1589c828b88b373b3c1ef29",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "dfa2a6fcc2983e705388",
+    "url": "/lottery/static/css/main.b67ed0f2.chunk.css"
+  },
+  {
+    "revision": "ec977a66d12fbec55824",
+    "url": "/lottery/static/js/2.7bf3252b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lottery/static/js/2.7bf3252b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfa2a6fcc2983e705388",
+    "url": "/lottery/static/js/main.e34429eb.chunk.js"
+  },
+  {
+    "revision": "c6d9fe75248c939ed15b",
+    "url": "/lottery/static/js/runtime-main.3f1a1b03.js"
+  }
+]);
